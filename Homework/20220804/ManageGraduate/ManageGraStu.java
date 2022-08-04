fe9809b33654b075395bef22a5053de9e1d26c0b
admin <admin@YD-22>
2022-08-04 17:39:59 +0900

    q

--- Homework/20220804/ManageGraduate/ManageGraStu.java
+++ Homework/20220804/ManageGraduate/ManageGraStu.java
@@ -1,5 +1,13 @@
 package ManageGraduate;
 
-public class ManageGraStu {
+import java.util.Scanner;
+
+import DBATEST.DAO;
+
+public class ManageGraStu extends DAO {
+	
+	//로그인
+	//db pw/id 존재여부 /동일여부
+	
 	
 }
